ee0e2181761650c689d2e69dca958578081b44bb
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-10-02 09:44:30 +0200

    Update exercice6

--- exercice6.php
+++ exercice6.php
@@ -1,17 +1,18 @@
 <?php
-  function order($array) {
-    sort($array);
-    return $array;
-  }
-  function glue($array) {
-    $lignes = implode(', ', $array);
-    return $lignes;
-  }
-  $langage = array("HTML","CSS","Javascript","PHP");
-  $result = order($langage);
-  foreach($result as $val) {
-      echo $val;
-  }
-  echo '<br />';
-  $collerserrer = glue($langage);
-      echo $collerserrer;
+function order($array) {
+  sort($array);
+  return $array;
+}
+function glue($array) {
+  $lignes = implode(', ', $array);
+  return $lignes;
+}
+$langage = array("HTML","CSS","Javascript","PHP");
+$result = order($langage);
+foreach($result as $val) {
+  echo $val;
+}
+echo '<br />';
+$collerserrer = glue($langage);
+echo $collerserrer;
+?>
